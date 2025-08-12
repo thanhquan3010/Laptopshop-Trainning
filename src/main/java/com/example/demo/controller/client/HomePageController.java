62f17928be4abffd59181eccda77867959a2b05a
thanhquan3010 <tranthanhquan09@gmail.com>
2025-08-12 14:09:34 +0700

    config homePage file in client

--- src/main/java/com/example/demo/controller/client/HomePageController.java
+++ src/main/java/com/example/demo/controller/client/HomePageController.java
@@ -1,5 +1,35 @@
 package com.example.demo.controller.client;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 public class HomePageController {
-    
+    // giao dien trang home
+    @GetMapping("/")
+    public String getHomePage() {
+        return null;
+    }
+
+    // dang ky
+    @GetMapping("/register")
+    public String getRegisterPage() {
+        return null;
+    }
+
+    @PostMapping("/register")
+    public String handleRegisterPage() {
+        return null;
+    }
+
+    // dang nhap
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return null;
+    }
+
+    @GetMapping("/access-deny")
+    public String getDenyPage() {
+        return null;
+    }
+
 }
