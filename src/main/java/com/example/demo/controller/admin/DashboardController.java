75381c778ba38a92c25acc87d5fe649ea82ab949
thanhquan3010 <tranthanhquan09@gmail.com>
2025-08-12 14:01:57 +0700

    config router order and dashboard page in admin

--- src/main/java/com/example/demo/controller/admin/DashboardController.java
+++ src/main/java/com/example/demo/controller/admin/DashboardController.java
@@ -1,5 +1,10 @@
 package com.example.demo.controller.admin;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class DashboardController {
-    
+    @GetMapping("/admin")
+    public String getDashBoard() {
+        return null;
+    }
 }
--- src/main/java/com/example/demo/controller/admin/OderController.java
+++ src/main/java/com/example/demo/controller/admin/OderController.java
@@ -1,5 +1,39 @@
 package com.example.demo.controller.admin;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 public class OderController {
-    
+    // xem giao dien gio hang
+    @GetMapping("/admin/order")
+    public String getDashboard() {
+        return null;
+    }
+
+    @GetMapping("/admin/order/{id}")
+    public String getViewOrderPage() {
+        return null;
+    }
+
+    // cap nhat thong tin gio hang
+    @GetMapping("/admin/order/update/{id}")
+    public String getUpdateOrderPage() {
+        return null;
+    }
+
+    @PostMapping("/admin/order/update")
+    public String handleUpdateOrderPage() {
+        return null;
+    }
+
+    // xoa san pham trong gio hang
+    @GetMapping("admin/order/delete/{id}")
+    public String getDeleteOrderPage() {
+        return null;
+    }
+
+    @PostMapping("/admin/order/delete")
+    public String handleDeleteOrderPage() {
+        return null;
+    }
 }
